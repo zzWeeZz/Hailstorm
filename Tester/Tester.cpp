e09f9335004f023b461ea0adbd60556e6e5afc2d
zzWeeZz <42831999+zzWeeZz@users.noreply.github.com>
2022-05-12 11:12:10 +0200

    init

---
+++ Tester/Tester.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Tester
+{
+	TEST_CLASS(Tester)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
